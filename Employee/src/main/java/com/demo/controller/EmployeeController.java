73f9ee8ad8e54c16d1694162016a8fa64d594875
VyenkteshMohite <mohitevyenktesh@gmail.com>
2024-02-29 21:21:49 +0530

    updated the code

---
+++ Employee/src/main/java/com/demo/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.model.Employee;
+import com.demo.repository.EmployeeRepository;
+
+import java.util.List;
+
+@RestController		
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping
+	public List<Employee> getAllEmployee(){
+		return employeeRepository.findAll();
+	}
+
+}
